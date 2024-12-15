893b10539e125d4e99210babda2c04853e681419
yankee-hedgehog <yankee.hedgehog.610@gmail.com>
2024-12-15 20:08:30 +0900

    add: Setting of prisma seed.

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,24 +2,61 @@ import prisma from '../lib/prisma'
 
 async function main() {
   const response = await Promise.all([
-    prisma.users.upsert({
-      where: { email: 'rauchg@vercel.com' },
+    prisma.kakeibo.upsert({
+      where: { id: 'bd0744e1-b63d-473e-925f-47efe373e300' },
       update: {},
       create: {
-        name: 'Guillermo Rauch',
-        email: 'rauchg@vercel.com',
-        image:
-          'https://images.ctfassets.net/e5382hct74si/2P1iOve0LZJRZWUzfXpi9r/9d4d27765764fb1ad7379d7cbe5f1043/ucxb4lHy_400x400.jpg',
+        id: 'bd0744e1-b63d-473e-925f-47efe373e300',
       },
     }),
-    prisma.users.upsert({
-      where: { email: 'lee@vercel.com' },
+    await prisma.category.upsert({
+      where: { id: '6ad449a0-6980-4923-aef5-376727e1ad76' },
       update: {},
       create: {
-        name: 'Lee Robinson',
-        email: 'lee@vercel.com',
-        image:
-          'https://images.ctfassets.net/e5382hct74si/4BtM41PDNrx4z1ml643tdc/7aa88bdde8b5b7809174ea5b764c80fa/adWRdqQ6_400x400.jpg',
+        id: '6ad449a0-6980-4923-aef5-376727e1ad76',
+        name: '交通費',
+        icon: 'car-front',
+        kakeiboId: 'bd0744e1-b63d-473e-925f-47efe373e300',
+      },
+    }),
+    await prisma.category.upsert({
+      where: { id: 'cdc15a32-ad40-449c-af84-723615684210' },
+      update: {},
+      create: {
+        id: 'cdc15a32-ad40-449c-af84-723615684210',
+        name: '夜ごはん',
+        icon: 'utensils-crossed',
+        kakeiboId: 'bd0744e1-b63d-473e-925f-47efe373e300',
+      },
+    }),
+    await prisma.category.upsert({
+      where: { id: 'f7495433-84ce-406e-85df-0f41fcc08701' },
+      update: {},
+      create: {
+        id: 'f7495433-84ce-406e-85df-0f41fcc08701',
+        name: '光熱費',
+        icon: 'lightbulb',
+        kakeiboId: 'bd0744e1-b63d-473e-925f-47efe373e300',
+      },
+    }),
+    await prisma.category.upsert({
+      where: { id: 'cdd77371-e036-487c-8b83-c9852994f6b1' },
+      update: {},
+      create: {
+        id: 'cdd77371-e036-487c-8b83-c9852994f6b1',
+        name: '昼ごはん',
+        icon: 'sandwich',
+        kakeiboId: 'bd0744e1-b63d-473e-925f-47efe373e300',
+      },
+    }),
+    await prisma.category.upsert({
+      where: { id: '2986b9d4-7496-4e71-a826-e763d7422d66' },
+      update: {},
+      create: {
+        id: '2986b9d4-7496-4e71-a826-e763d7422d66',
+        name: '趣味',
+        icon: 'tree-palm',
+        kakeiboId: 'bd0744e1-b63d-473e-925f-47efe373e300',
       },
     }),
     await prisma.users.upsert({
