f7de7556d0c320ba38aec6b586aa97fc5c227786
yankee-hedgehog <yankee.hedgehog.610@gmail.com>
2025-01-03 10:06:20 +0900

    fix: Changing default page title.

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,3 @@
-import { Button } from '@/components/ui/button'
-
-const data = [
-  { name: 'a', value: 1000 },
-  { name: 'b', value: 1500 },
-  { name: 'c', value: 2000 },
-  { name: 'd', value: 2500 },
-]
-
 export default function Page() {
-  return <Button>a</Button>
+  return <>kakeibo!</>
 }
